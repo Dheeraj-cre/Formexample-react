23cba905cb61d21a5017c7f386bfde12f92624d2
Dheeraj-cre <Dheerajn2311@gmail.com>
2025-08-21 23:27:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Textfieldexample from './Textfieldexample' 
+import Faculty from './Faculty'
+
+function App() {
+  return(
+          <div>
+            <Textfieldexample/>
+            <Faculty/>
+           
+          </div>
+    )
+
+}
+
+export default App
